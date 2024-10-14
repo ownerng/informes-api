71b9b657e82605bc86cb5f47bb72dfde35bb4852
Jhonnatan Mendez <jhonnatandavid34mc@gmail.com>
2024-10-14 13:25:42 -0500

    modified services and creating data.json

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:5173', // Orígenes permitidos
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Métodos HTTP permitidos
+    credentials: true, // Permitir el envío de cookies y credenciales
+  });
+
   await app.listen(3000);
 }
 bootstrap();
--- src/select/services/select/select.service.ts
+++ src/select/services/select/select.service.ts
@@ -1,35 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import * as data from '../../../common/data.json';
 
 @Injectable()
 export class SelectService {
   
   // Obtener la lista de lugares
   getLugares(): string[] {
-    return [
-      'Uribe',
-      'Puerto Lleras',
-      'La Macarena',
-      'Puerto Concordia',
-      'Restrepo',
-      'Mesetas',
-      'San Juan de Arama',
-      'Cumaral',
-      'Vista Hermosa',
-      'Mapiripán',
-      'PIC Departamental',
-      'Barranca de Upía',
-    ];
+    return data.lugares;
   }
 
   // Obtener la lista de entregas items
   getEntregasItems(): string[] {
-    return [
-      'Perifoneo',
-      'Almuerzos',
-      'Refrigerios',
-      'Sillas',
-      'Mesas',
-      'Carpa',
-    ];
+    return data.entregasItems;
   }
 }
